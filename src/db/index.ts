affa91c724240f13081aa46516eee67119e1a5b8
tloyan <thomas.loyan@gmail.com>
2025-12-11 15:57:50 +0100

    config and add better-auth with email credential and social credential (github and google)

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import * as articles from "./schema/articles";
+import * as auth from "./schema/auth-schema";
 
-export default drizzle({ client: sql, schema: { ...articles } });
+export default drizzle({ client: sql, schema: { ...articles, ...auth } });
